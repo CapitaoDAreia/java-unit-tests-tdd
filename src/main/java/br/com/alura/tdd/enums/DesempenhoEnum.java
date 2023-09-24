096ee43237a0e6443436c0a22af2b450ac8da60a
CapitaoDAreia <danieligord7@hotmail.com>
2023-09-24 15:52:16 -0300

    Refactoring applying strategy

--- src/main/java/br/com/alura/tdd/enums/DesempenhoEnum.java
+++ src/main/java/br/com/alura/tdd/enums/DesempenhoEnum.java
@@ -1,7 +1,26 @@
 package br.com.alura.tdd.enums;
 
+import java.math.BigDecimal;
+
 public enum DesempenhoEnum {
-    A_DESEJAR,
-    BOM,
-    OTIMO
+    A_DESEJAR {
+        @Override
+        public BigDecimal calcularAliquota() {
+            return new BigDecimal("0.03");
+        }
+    },
+    BOM {
+        @Override
+        public BigDecimal calcularAliquota() {
+            return new BigDecimal("0.15");
+        }
+    },
+    OTIMO {
+        @Override
+        public BigDecimal calcularAliquota() {
+            return new BigDecimal("0.20");
+        }
+    };
+
+    public abstract BigDecimal calcularAliquota();
 }
--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -3,28 +3,14 @@ package br.com.alura.tdd.service;
 import br.com.alura.tdd.enums.DesempenhoEnum;
 import br.com.alura.tdd.modelo.Funcionario;
 
-import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class ReajusteService {
     public void aplicarReajuste(Funcionario empregado, DesempenhoEnum desempenho) {
         var salarioAtual = empregado.getSalario();
-        var aliquota = calcularAliquota(desempenho);
+        var aliquota = desempenho.calcularAliquota();
         var salarioReajustado = salarioAtual.add(salarioAtual.multiply(aliquota)).setScale(2, RoundingMode.HALF_UP);
 
         empregado.reajustarSalario(salarioReajustado);
-    }
-
-    private BigDecimal calcularAliquota(DesempenhoEnum desempenho) {
-        switch (desempenho){
-            case A_DESEJAR:
-                return new BigDecimal("0.03");
-            case BOM:
-                return new BigDecimal("0.15");
-            case OTIMO:
-                return new BigDecimal("0.20");
-            default:
-                return null;
-        }
     }
 }
